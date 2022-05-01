45b1c1cac48033b9f9ffdae654e305ae9a381828
Els <els52025099@gmail.com>
2022-05-01 22:36:46 +0900

    add filtering

--- read.py
+++ read.py
@@ -20,4 +20,10 @@ print('檔案讀取完畢,共有', len(data), '筆資料')
 print('平均評論長度: ', average_len)
 
 
+new = []
+for d in data: #每個d為一個單獨的評論
+	if len(d) < 100: #把長度<100的評論存起來
+		new.append(d)
+print('一共有', len(new), '比留言長度小於100')
+
 
